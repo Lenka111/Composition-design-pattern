e6410948a1268441cb982a2fc9af11fb4a1d7dd8
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-04-02 13:39:06 -0500

    Add files via upload

---
+++ Bathroom.java
@@ -0,0 +1,20 @@
+
+public class Bathroom {
+private Tub bathtub;
+
+public Bathroom() {
+	bathtub = new Tub(12);
+	
+}
+
+public int getTubSize() {
+	return bathtub.getSize();
+}
+public String getBubbleColor() {
+	return bathtub.getBubbleColor();
+}
+public int getBubbleRadius() {
+	return bathtub.getBubbleRadius();
+}
+}
+
